48fa71d285dd429631f8558c55fd73c6d75cf8d4
paulrudy <1110792+paulrudy@users.noreply.github.com>
2022-10-17 21:51:44 -0700

    add shadow dom

--- tree-select.js
+++ tree-select.js
@@ -56,83 +56,94 @@
   };
 
   function buildPopup() {
-    popupEl = document.createElement("div");
-    popupEl.setAttribute("id", props.uniqueId);
+    class PopUpInfo extends HTMLElement {
+      constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: "closed" });
 
-    Object.assign(popupEl.style, {
-      position: "fixed",
-      left: windowPosition.left + "px",
-      top: windowPosition.top + "px",
-      zIndex: Number.MAX_SAFE_INTEGER,
-      width: props.width,
-      minWidth: props.minWidth,
-      background: props.background,
-      color: props.color,
-      opacity: props.opacity,
-      borderRadius: props.borderRadius + "em",
-      border: props.borderThickness + "px solid " + props.color,
-    });
-    popupHeader = document.createElement("div");
-    Object.assign(popupHeader.style, {
-      padding: "0 .25em .25em 1em",
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-    });
-    const popupHeaderTitle = document.createElement("div");
-    Object.assign(popupHeaderTitle.style, {});
-    popupHeaderTitle.textContent = props.windowTitle;
-    closeButton = document.createElement("button");
-    closeButton.setAttribute("id", props.uniqueId + "__close-button");
+        popupEl = document.createElement("div");
+        popupEl.setAttribute("id", props.uniqueId);
 
-    popupBody = document.createElement("div");
-    Object.assign(popupBody.style, {
-      padding: ".25em 1em",
-      display: "grid",
-      rowGap: ".25em",
-    });
-    const p = document.createElement("p");
-    p.textContent = "Choose node level for selection";
-    popupBody.appendChild(p);
-    const upButton = document.createElement("button");
-    upButton.innerHTML = "Up";
-    upButton.onclick = () => {
-      index = selectNodeContents(index + 1) ?? index;
-    };
-    const downButton = document.createElement("button");
-    downButton.innerHTML = "Down";
-    downButton.onclick = () => {
-      index = selectNodeContents(index - 1) ?? index;
-    };
-    popupBody.appendChild(upButton);
-    popupBody.appendChild(downButton);
-    closeButton.addEventListener("click", () => popupEl.remove(), false);
-    closeButton.textContent = "×";
-    popupHeader.appendChild(popupHeaderTitle);
-    popupHeader.appendChild(closeButton);
-    popupEl.appendChild(popupHeader);
-    popupEl.appendChild(popupBody);
-    document.body.appendChild(popupEl);
-    document.querySelectorAll(`#${props.uniqueId} button`).forEach((b) =>
-      Object.assign(b.style, {
-        cursor: "pointer",
-        background: "none",
-        padding: ".25em",
-        border: props.borderThickness + "px solid " + props.color,
-        color: props.color,
-        borderRadius: ".25em",
-      })
-    );
-    Object.assign(closeButton.style, {
-      aspectRatio: "1",
-      color: props.color,
-      textAlign: "center",
-      padding: "0",
-      border: "none",
-    });
+        Object.assign(popupEl.style, {
+          position: "fixed",
+          left: windowPosition.left + "px",
+          top: windowPosition.top + "px",
+          zIndex: Number.MAX_SAFE_INTEGER,
+          width: props.width,
+          minWidth: props.minWidth,
+          background: props.background,
+          color: props.color,
+          opacity: props.opacity,
+          borderRadius: props.borderRadius + "em",
+          border: props.borderThickness + "px solid " + props.color,
+        });
+        popupHeader = document.createElement("div");
+        Object.assign(popupHeader.style, {
+          padding: "0 .25em .25em 1em",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        });
+        const popupHeaderTitle = document.createElement("div");
+        Object.assign(popupHeaderTitle.style, {});
+        popupHeaderTitle.textContent = props.windowTitle;
+        closeButton = document.createElement("button");
+        closeButton.setAttribute("id", props.uniqueId + "__close-button");
+
+        popupBody = document.createElement("div");
+        Object.assign(popupBody.style, {
+          padding: ".25em 1em",
+          display: "grid",
+          rowGap: ".25em",
+        });
+        const p = document.createElement("p");
+        p.textContent = "Choose node level for selection";
+        popupBody.appendChild(p);
+        const upButton = document.createElement("button");
+        upButton.innerHTML = "Up";
+        upButton.onclick = () => {
+          index = selectNodeContents(index + 1) ?? index;
+        };
+        const downButton = document.createElement("button");
+        downButton.innerHTML = "Down";
+        downButton.onclick = () => {
+          index = selectNodeContents(index - 1) ?? index;
+        };
+        popupBody.appendChild(upButton);
+        popupBody.appendChild(downButton);
+        closeButton.addEventListener("click", () => popupEl.remove(), false);
+        closeButton.textContent = "×";
+        popupHeader.appendChild(popupHeaderTitle);
+        popupHeader.appendChild(closeButton);
+        popupEl.appendChild(popupHeader);
+        popupEl.appendChild(popupBody);
+        shadow.appendChild(popupEl);
+
+        document.querySelectorAll(`#${props.uniqueId} button`).forEach((b) =>
+          Object.assign(b.style, {
+            cursor: "pointer",
+            background: "none",
+            padding: ".25em",
+            border: props.borderThickness + "px solid " + props.color,
+            color: props.color,
+            borderRadius: ".25em",
+          })
+        );
+        Object.assign(closeButton.style, {
+          aspectRatio: "1",
+          color: props.color,
+          textAlign: "center",
+          padding: "0",
+          border: "none",
+        });
+      }
+    }
+    customElements.define("popup-info", PopUpInfo);
   }
-
   buildPopup();
+
+  let test = document.createElement("popup-info");
+  document.body.appendChild(test);
 
   draggable(popupHeader);
 
